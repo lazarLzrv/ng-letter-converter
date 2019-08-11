e3fd3a4bf7347fe65f5e9db4af30f3b8fdbdef5e
Lazar Lazarov <lazar.lzrv@gmail.com>
2019-08-12 00:05:21 +0200

    number to letter func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -8,18 +8,29 @@ import { Component} from '@angular/core';
 
 export class AppComponent {
     letters:string;
-    resultNumbers :number = 0;
- 
- 
+    resultNumbers :number = 0; 
 
-    convertToNumber = () =>{  
-        for (var i = 0; i < this.letters.length; i++) {
-            this.resultNumbers += (this.letters.charCodeAt(i) - 64) * Math.pow(26, this.letters.length - i - 1);
-        }  
-        console.log(this.resultNumbers); 
+    numbers:number; 
+    numbersInst:number; 
+    multiplier:number = 26; 
+    resultLetters:string='';  
+    tempVar:number = 1;
+
+    convertToNumber = () => {  
+        this.resultNumbers=0; 
+        for (let i = 0; this.letters.length > i ; i++) {
+    
+            this.resultNumbers += (this.letters.toUpperCase().charCodeAt(i) - 64) * Math.pow(26, this.letters.length - i - 1); 
+        }    
     } 
 
-    convertToLetter = () =>{
-        
+    convertToLetter = () => {  
+        this.resultLetters='';  
+
+        this.numbersInst = this.numbers;
+        for (this.tempVar=1 , this.multiplier=26; (this.numbersInst -= this.tempVar) >= 0; this.tempVar = this.multiplier, this.multiplier *= 26) {
+            this.resultLetters = String.fromCharCode(((this.numbersInst % this.multiplier) / this.tempVar) + 65) + this.resultLetters;
+        }  
     }
+   
 }
