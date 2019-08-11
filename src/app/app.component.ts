977ec24193f3c2a933755d17be89d968538abf4c
Lazar Lazarov <lazar.lzrv@gmail.com>
2019-08-11 21:17:20 +0200

    letter to number func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'letter';
+    letters:string;
+    resultNumbers :number = 0;
+ 
+ 
+
+    convertToNumber = () =>{  
+        for (var i = 0; i < this.letters.length; i++) {
+            this.resultNumbers += (this.letters.charCodeAt(i) - 64) * Math.pow(26, this.letters.length - i - 1);
+        }  
+        console.log(this.resultNumbers); 
+    } 
+
+    convertToLetter = () =>{
+        
+    }
 }
